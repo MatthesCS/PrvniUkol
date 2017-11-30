a0c87b1dc9149083df14df9e8a633bfd6420d976
MatthesCS <Matej.Fries@seznam.cz>
2017-11-30 12:39:14 +0100

    Přidána proměnná pocetBodu na nastavení počtu bodů v gridu
    Tento počet se mění klávesami 8 a 9 na num. klávesnici v rozsahu od 4 do 100.
    
    NetBeans.

--- PrvniUkol/src/app/Renderer.java
+++ PrvniUkol/src/app/Renderer.java
@@ -30,8 +30,7 @@ import utils.MeshGenerator;
  * @since 2015-09-05
  */
 public class Renderer implements GLEventListener, MouseListener,
-        MouseMotionListener, KeyListener
-{
+        MouseMotionListener, KeyListener {
 
     int width, height, ox, oy;
 
@@ -42,9 +41,8 @@ public class Renderer implements GLEventListener, MouseListener,
     int shaderProgram, locMat, locSvetlo, locOko;
     int gridShaderProgram, gridLocMat, gridLocSvetlo, gridLocOko, gridLocPoziceSvetel;
     int gridLocDifBarva, gridLocSpecBarva, gridLocAmbBarva, gridLocPrimBarva;
-    int svetlo;
+    int svetlo, pocetBodu = 10;
 
-    
     Camera cam = new Camera();
     Mat4 proj; // created in reshape()
     Vec3D poziceOka, difuzniBarvaSvetla, specularniBarvaSvetla, ambientniBarvaSvetla, primaBarvaSvetla;
@@ -54,8 +52,7 @@ public class Renderer implements GLEventListener, MouseListener,
     OGLTexture2D.Viewer textureViewer;
 
     @Override
-    public void init(GLAutoDrawable glDrawable)
-    {
+    public void init(GLAutoDrawable glDrawable) {
         // check whether shaders are supported
         GL2GL3 gl = glDrawable.getGL().getGL2GL3();
         OGLUtils.shaderCheck(gl);
@@ -63,7 +60,7 @@ public class Renderer implements GLEventListener, MouseListener,
         // get and set debug version of GL class
         gl = OGLUtils.getDebugGL(gl);
         glDrawable.setGL(gl);
-        
+
         OGLUtils.printOGLparameters(gl);
 
         textRenderer = new OGLTextRenderer(gl, glDrawable.getSurfaceWidth(), glDrawable.getSurfaceHeight());
@@ -76,12 +73,11 @@ public class Renderer implements GLEventListener, MouseListener,
         locSvetlo = gl.glGetUniformLocation(shaderProgram, "svetlo");
         locOko = gl.glGetUniformLocation(shaderProgram, "oko");
 
-
         gridLocMat = gl.glGetUniformLocation(gridShaderProgram, "mat");
         gridLocSvetlo = gl.glGetUniformLocation(gridShaderProgram, "svetlo");
         gridLocOko = gl.glGetUniformLocation(gridShaderProgram, "oko");
-        gridLocPoziceSvetel  = gl.glGetUniformLocation(gridShaderProgram, "svetlaPozice");
-        
+        gridLocPoziceSvetel = gl.glGetUniformLocation(gridShaderProgram, "svetlaPozice");
+
         gridLocDifBarva = gl.glGetUniformLocation(gridShaderProgram, "difBarva");
         gridLocSpecBarva = gl.glGetUniformLocation(gridShaderProgram, "specBarva");
         gridLocAmbBarva = gl.glGetUniformLocation(gridShaderProgram, "ambBarva");
@@ -90,7 +86,6 @@ public class Renderer implements GLEventListener, MouseListener,
         cam = cam.withPosition(new Vec3D(5, 5, 2.5))
                 .withAzimuth(Math.PI * 1.25)
                 .withZenith(Math.PI * -0.125);
-        
 
         gl.glEnable(GL2GL3.GL_DEPTH_TEST);
 
@@ -98,64 +93,61 @@ public class Renderer implements GLEventListener, MouseListener,
         textureViewer = new OGLTexture2D.Viewer(gl);
     }
 
-    void createBuffers(GL2GL3 gl)
-    {        
-        grid = MeshGenerator.generateGrid(10, 10, gl, "inPosition");
-        float[] cube =
-                {
-                        // bottom (z-) face
-                        1, 0, 0, 0, 0, -1,
-                        0, 0, 0, 0, 0, -1,
-                        1, 1, 0, 0, 0, -1,
-                        0, 1, 0, 0, 0, -1,
-                        // top (z+) face
-                        1, 0, 1, 0, 0, 1,
-                        0, 0, 1, 0, 0, 1,
-                        1, 1, 1, 0, 0, 1,
-                        0, 1, 1, 0, 0, 1,
-                        // x+ face
-                        1, 1, 0, 1, 0, 0,
-                        1, 0, 0, 1, 0, 0,
-                        1, 1, 1, 1, 0, 0,
-                        1, 0, 1, 1, 0, 0,
-                        // x- face
-                        0, 1, 0, -1, 0, 0,
-                        0, 0, 0, -1, 0, 0,
-                        0, 1, 1, -1, 0, 0,
-                        0, 0, 1, -1, 0, 0,
-                        // y+ face
-                        1, 1, 0, 0, 1, 0,
-                        0, 1, 0, 0, 1, 0,
-                        1, 1, 1, 0, 1, 0,
-                        0, 1, 1, 0, 1, 0,
-                        // y- face
-                        1, 0, 0, 0, -1, 0,
-                        0, 0, 0, 0, -1, 0,
-                        1, 0, 1, 0, -1, 0,
-                        0, 0, 1, 0, -1, 0
+    void createBuffers(GL2GL3 gl) {
+        grid = MeshGenerator.generateGrid(pocetBodu, pocetBodu, gl, "inPosition");
+        float[] cube
+                = {
+                    // bottom (z-) face
+                    1, 0, 0, 0, 0, -1,
+                    0, 0, 0, 0, 0, -1,
+                    1, 1, 0, 0, 0, -1,
+                    0, 1, 0, 0, 0, -1,
+                    // top (z+) face
+                    1, 0, 1, 0, 0, 1,
+                    0, 0, 1, 0, 0, 1,
+                    1, 1, 1, 0, 0, 1,
+                    0, 1, 1, 0, 0, 1,
+                    // x+ face
+                    1, 1, 0, 1, 0, 0,
+                    1, 0, 0, 1, 0, 0,
+                    1, 1, 1, 1, 0, 0,
+                    1, 0, 1, 1, 0, 0,
+                    // x- face
+                    0, 1, 0, -1, 0, 0,
+                    0, 0, 0, -1, 0, 0,
+                    0, 1, 1, -1, 0, 0,
+                    0, 0, 1, -1, 0, 0,
+                    // y+ face
+                    1, 1, 0, 0, 1, 0,
+                    0, 1, 0, 0, 1, 0,
+                    1, 1, 1, 0, 1, 0,
+                    0, 1, 1, 0, 1, 0,
+                    // y- face
+                    1, 0, 0, 0, -1, 0,
+                    0, 0, 0, 0, -1, 0,
+                    1, 0, 1, 0, -1, 0,
+                    0, 0, 1, 0, -1, 0
                 };
 
         poziceOka = cam.getEye();
-        
+
         poziceSvetel.add(new Vec3D(0, 0, 10));
         poziceSvetel.add(new Vec3D(5, 5, -3));
 
         svetlo = 0;
         difuzniBarvaSvetla = new Vec3D(0.8, 0.9, 0.6);
         specularniBarvaSvetla = new Vec3D(1.0, 1.0, 1.0);
-        ambientniBarvaSvetla = new Vec3D(0.3,0.1,0.5);
+        ambientniBarvaSvetla = new Vec3D(0.3, 0.1, 0.5);
         primaBarvaSvetla = new Vec3D(1.0, 0.9, 0.9);
-        
+
         /*
         difuzniBarvaSvetla = new Vec3D(0.6, 0.6, 0.6);//co sežere matroš
         specularniBarvaSvetla = new Vec3D(1.0, 1.0, 1.0);//odražečná
         ambientniBarvaSvetla = new Vec3D(0.3,0.3,0.3);//odraz?
         primaBarvaSvetla = new Vec3D(0.9, 0.9, 0.9);//světlo
-        */
-
+         */
         int[] indexBufferData = new int[36];
-        for (int i = 0; i < 6; i++)
-        {
+        for (int i = 0; i < 6; i++) {
             indexBufferData[i * 6] = i * 4;
             indexBufferData[i * 6 + 1] = i * 4 + 1;
             indexBufferData[i * 6 + 2] = i * 4 + 2;
@@ -163,26 +155,26 @@ public class Renderer implements GLEventListener, MouseListener,
             indexBufferData[i * 6 + 4] = i * 4 + 2;
             indexBufferData[i * 6 + 5] = i * 4 + 3;
         }
-        OGLBuffers.Attrib[] attributes =
-                {
-                        new OGLBuffers.Attrib("inPosition", 3),
-                        new OGLBuffers.Attrib("inNormal", 3)
+        OGLBuffers.Attrib[] attributes
+                = {
+                    new OGLBuffers.Attrib("inPosition", 3),
+                    new OGLBuffers.Attrib("inNormal", 3)
                 };
 
         buffers = new OGLBuffers(gl, cube, attributes, indexBufferData);
     }
 
     @Override
-    public void display(GLAutoDrawable glDrawable)
-    {
+    public void display(GLAutoDrawable glDrawable) {
+        
+        grid = MeshGenerator.generateGrid(pocetBodu, pocetBodu, glDrawable.getGL().getGL2GL3(), "inPosition");
         poziceOka = cam.getEye();
         GL2GL3 gl = glDrawable.getGL().getGL2GL3();
 
         gl.glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         gl.glClear(GL2GL3.GL_COLOR_BUFFER_BIT | GL2GL3.GL_DEPTH_BUFFER_BIT);
 
-        if(k)
-        {
+        if (k) {
             gl.glUseProgram(shaderProgram);
             gl.glUniformMatrix4fv(locMat, 1, false,
                     ToFloatArray.convert(cam.getViewMatrix().mul(proj)), 0);
@@ -200,21 +192,19 @@ public class Renderer implements GLEventListener, MouseListener,
         gl.glUniform3fv(gridLocAmbBarva, 1, ToFloatArray.convert(ambientniBarvaSvetla), 0);
         gl.glUniform3fv(gridLocDifBarva, 1, ToFloatArray.convert(difuzniBarvaSvetla), 0);
         gl.glUniform3fv(gridLocSpecBarva, 1, ToFloatArray.convert(specularniBarvaSvetla), 0);
-        gl.glUniform3fv(gridLocPrimBarva, 1, ToFloatArray.convert(primaBarvaSvetla), 0);        
-        
+        gl.glUniform3fv(gridLocPrimBarva, 1, ToFloatArray.convert(primaBarvaSvetla), 0);
+
         gl.glUniform1f(gridLocSvetlo, svetlo);
 
         texture.bind(shaderProgram, "textureID", 0);
 
-        if(poly)
-        {
+        if (poly) {
             gl.glPolygonMode(GL2GL3.GL_FRONT_AND_BACK, GL2GL3.GL_LINE);
         }
 
         grid.draw(GL2GL3.GL_TRIANGLES, gridShaderProgram);
 
         //textureViewer.view(texture, -1, -1, 0.5);
-
         String text = new String(this.getClass().getName() + ": [LMB] camera, WSAD");
 
         textRenderer.drawStr2D(3, height - 20, text);
@@ -223,8 +213,7 @@ public class Renderer implements GLEventListener, MouseListener,
 
     @Override
     public void reshape(GLAutoDrawable drawable, int x, int y, int width,
-                        int height)
-    {
+            int height) {
         this.width = width;
         this.height = height;
         proj = new Mat4PerspRH(Math.PI / 4, height / (double) width, 0.01, 1000.0);
@@ -232,35 +221,29 @@ public class Renderer implements GLEventListener, MouseListener,
     }
 
     @Override
-    public void mouseClicked(MouseEvent e)
-    {
+    public void mouseClicked(MouseEvent e) {
     }
 
     @Override
-    public void mouseEntered(MouseEvent e)
-    {
+    public void mouseEntered(MouseEvent e) {
     }
 
     @Override
-    public void mouseExited(MouseEvent e)
-    {
+    public void mouseExited(MouseEvent e) {
     }
 
     @Override
-    public void mousePressed(MouseEvent e)
-    {
+    public void mousePressed(MouseEvent e) {
         ox = e.getX();
         oy = e.getY();
     }
 
     @Override
-    public void mouseReleased(MouseEvent e)
-    {
+    public void mouseReleased(MouseEvent e) {
     }
 
     @Override
-    public void mouseDragged(MouseEvent e)
-    {
+    public void mouseDragged(MouseEvent e) {
         cam = cam.addAzimuth((double) Math.PI * (ox - e.getX()) / width)
                 .addZenith((double) Math.PI * (e.getY() - oy) / width);
         ox = e.getX();
@@ -268,15 +251,12 @@ public class Renderer implements GLEventListener, MouseListener,
     }
 
     @Override
-    public void mouseMoved(MouseEvent e)
-    {
+    public void mouseMoved(MouseEvent e) {
     }
 
     @Override
-    public void keyPressed(KeyEvent e)
-    {
-        switch (e.getKeyCode())
-        {
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
             case KeyEvent.VK_W:
             case KeyEvent.VK_UP:
                 cam = cam.forward(1);
@@ -316,27 +296,36 @@ public class Renderer implements GLEventListener, MouseListener,
                 break;
             case KeyEvent.VK_L:
                 svetlo++;
-                if (svetlo > 3)
-                {
+                if (svetlo > 3) {
                     svetlo = 0;
                 }
                 break;
+            case KeyEvent.VK_NUMPAD9:
+                if (pocetBodu < 100) 
+                {
+                    pocetBodu++;
+                }
+                break;
+            case KeyEvent.VK_NUMPAD8:
+                if (pocetBodu > 4) 
+                {
+                    pocetBodu--;
+                }
+                break;
+
         }
     }
 
     @Override
-    public void keyReleased(KeyEvent e)
-    {
+    public void keyReleased(KeyEvent e) {
     }
 
     @Override
-    public void keyTyped(KeyEvent e)
-    {
+    public void keyTyped(KeyEvent e) {
     }
 
     @Override
-    public void dispose(GLAutoDrawable glDrawable)
-    {
+    public void dispose(GLAutoDrawable glDrawable) {
         glDrawable.getGL().getGL2GL3().glDeleteProgram(shaderProgram);
     }
 
